5aff52fcead4a1afb3bc2d358b97dfad91e93009
Tutmirleid <valeriyjobemail@gmail.com>
2024-06-25 12:57:40 +0300

    добавил хедер и футер error.php

--- crud/error.php
+++ crud/error.php
@@ -1,5 +1,13 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/crud/header.php";
+?>
+
 <main>
     <div class='container'>
         <h2>Smth went wrong...</h2>
     </div>
 </main>
+
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/crud/footer.php";
+?>
